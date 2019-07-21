a6b4525a4e25b0454fc18843fc7dd341ea4609ec
Davidmue <d.mueller@ertl.ch>
2019-07-21 21:54:14 +0200

    Using Console Template

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gb2cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
